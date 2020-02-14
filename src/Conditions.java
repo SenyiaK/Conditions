7f5a16971e7ff115bfcd13f6f95322b23964cc05
SenyiaK <karzakova.92@mail.ru>
2020-02-14 11:07:23 +0700

    first commit

---
+++ src/Conditions.java
@@ -0,0 +1,17 @@
+public class Conditions {
+
+    public static void main (String[] args)
+    {
+        int x =1;
+        while(x<2)
+        {
+            System.out.println("Doo");
+            System.out.println("Bee");
+            x=x+1;
+        }
+        if (x==2)
+        {
+            System.out.println("Do");
+        }
+    }
+}
